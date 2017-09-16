9f90e2542092625cb483aa72bec05e9549244e33
rhclayto <h@obscur.us>
2017-09-16 14:03:22 -0600

    Allow specifying an ALSA device
    Allow specifying an ALSA device in the constructor options. This permits sending the PCM stream to a particular ALSA device.

--- src/binding.cc
+++ src/binding.cc
@@ -30,6 +30,10 @@ NAN_METHOD(Open) {
   ao->channels = info[1]->Int32Value(); /* channels */
   ao->rate = info[2]->Int32Value(); /* sample rate */
   ao->format = info[3]->Int32Value(); /* MPG123_ENC_* format */
+	// ao->device = (char*)"bluealsa:HCI=hci0,DEV=00:6A:8E:16:C5:F2,PROFILE=sco"; /* device */
+	// Thank god for https://stackoverflow.com/a/30929476 .
+	String::Utf8Value str(info[4]->ToString());
+	ao->device = (char*)*str;
 
   /* init_output() */
   r = mpg123_output_module_info.init_output(ao);
